097a7a924dbe450a55173336cb2173c8978f22a8
ASCIIbreakdown <td3of4@gmail.com>
2016-12-11 17:02:49 -0500

    removed lock icon

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-icon=/home/thom/icon.png
 tmpbg=/tmp/screen.png
 scrot /tmp/screen.png
 convert $tmpbg -blur 0x20 $tmpbg;
-convert $tmpbg $icon -gravity center -composite -matte $tmpbg
 i3lock -i $tmpbg
