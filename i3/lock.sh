f265b0c96812e1bf1f092538127bf01828430bac
ASCIIbreakdown <td3of4@gmail.com>
2016-12-12 17:57:29 -0500

    add lock icon

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 tmpbg=/tmp/screen.png
+icon=~/.config/i3/lock.png
 scrot /tmp/screen.png
 notify-send Locking...
 convert $tmpbg -blur 0x20 $tmpbg;
+convert $tmpbg $icon -gravity center -composite -matte $tmpbg
 i3lock -i $tmpbg
