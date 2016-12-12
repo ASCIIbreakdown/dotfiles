286b8908f8cc742c30667a1b8caaa3bec03c13e4
ASCIIbreakdown <td3of4@gmail.com>
2016-12-12 18:59:51 -0500

    improve notify-send command

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 tmpbg=/tmp/screen.png
 icon=~/.config/i3/lock.png
-scrot /tmp/screen.png
-notify-send Locking...
+scrot $tmpbg
+notify-send -i ~/.config/i3/lock.png -t 1000 "Locking..." "Locking your computer"
 convert $tmpbg -blur 0x20 $tmpbg;
 convert $tmpbg $icon -gravity center -composite -matte $tmpbg
 i3lock -i $tmpbg
